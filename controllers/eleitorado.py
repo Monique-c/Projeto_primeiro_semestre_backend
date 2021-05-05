4b24b9dc285d5c315f3d08f1a7b9f1241ed9a202
Gabriel Camargo Leite <55204419+GabrielCamargoL@users.noreply.github.com>
2021-05-05 12:59:07 -0300

    Feature/eleitorado (#2)
    * retornando dados de eleitado em um SELECT somente com comparaçao entre cidades
    
    * inclusao de faixa etaria por municipio (em progresso)
    
    * adiçao da faixa etaria no response JSON original separando por municipio.
    
    * fix da verificaçao de cidade faixa etaria e eleitorado + criaçao de arquivos para renda e relevantes
    
    * acoplados grau de escolaridade e estado civil no codigo de eleitorado
    
    * .pyc no gitignore

--- controllers/eleitorado.py
+++ controllers/eleitorado.py
@@ -6,7 +6,8 @@ import sys
 
 def connectDb(request):
 	json_data = request.get_json()
-	municipios = json_data["MN_MUNICIPIO"]
+	municipios = json_data["municipios"]
+	colunas = json_data["colunas"]
 
 # Conexão com mysql
 	try:
@@ -23,12 +24,33 @@ def connectDb(request):
 		print(f"Erro de conexão ao SGBD {e}")
 		sys.exit(1)
 
-	colunas = json_data["colunas"]
 	with connection:
 		with connection.cursor() as cursor:
-			retornoBanco = function.buscaPorMunicipiosComColunas(cursor, municipios, colunas)
+			retornoBancoEleitorado = function.buscaPorMunicipiosComColunas(cursor, municipios, colunas)
+			retornoBancoFaixaEtaria = function.buscaFaixaEtáriaPorMunicipio(cursor, municipios)
+			retornoBancoEstadoCivil = function.buscaEstadoCivilPorMunicipio(cursor, municipios)
+			retornoBancoGrauEscolaridade = function.buscaGrauEscolaridadePorMunicipio(cursor, municipios)
 
-	return json.dumps(retornoBanco)
+			for x in range(len(municipios)):
+				retornoBancoEleitorado[x]['faixa_etaria'] = []
+				retornoBancoEleitorado[x]['estado_civil'] = []
+				retornoBancoEleitorado[x]['grau_escolaridade'] = []
+
+			for idx in range(len(municipios)):
+				for idades_perfil in retornoBancoFaixaEtaria:
+					if idades_perfil['municipio'] == retornoBancoEleitorado[idx]['municipio']:
+						retornoBancoEleitorado[idx]['faixa_etaria'].append(idades_perfil)
+				
+				for estado_civil in retornoBancoEstadoCivil:
+					if estado_civil['municipio'] == retornoBancoEleitorado[idx]['municipio']:
+						retornoBancoEleitorado[idx]['estado_civil'].append(estado_civil)
+				
+				for grau_escolaridade in retornoBancoGrauEscolaridade:
+					if grau_escolaridade['municipio'] == retornoBancoEleitorado[idx]['municipio']:
+						retornoBancoEleitorado[idx]['grau_escolaridade'].append(grau_escolaridade)
+			
+
+	return json.dumps(retornoBancoEleitorado)
 
 def eleitoradoQuery(request):
 	return connectDb(request)
--- controllers/utils/queriesEleitorado.py
+++ controllers/utils/queriesEleitorado.py
@@ -2,20 +2,62 @@ import simplejson as json
 import sys
 import decimal
 
+def buscaFaixaEtáriaPorMunicipio(cursor, municipios:[]):
+	inMunicipios = ", ".join(map(lambda municipio: "'"+municipio+"'", municipios)) + " "
+
+	query = "SELECT `NM_MUNICIPIO` AS 'municipio', \
+					`CD_FAIXA_ETARIA` AS 'cd_faixa_etaria', `DS_FAIXA_ETARIA` AS 'desc_faixa_etaria', \
+					 SUM(QT_ELEITORES_PERFIL) AS 'soma_eleitores_perfil' \
+					FROM `eleitorado_ATUAL` " +\
+					"WHERE `NM_MUNICIPIO` IN(" + inMunicipios + ") " +\
+					"GROUP BY NM_MUNICIPIO, CD_FAIXA_ETARIA, DS_FAIXA_ETARIA \
+					ORDER BY NM_MUNICIPIO ASC"
+
+	return buscarResultadoPara(query, cursor)
+
+def buscaEstadoCivilPorMunicipio(cursor, municipios:[]):
+	inMunicipios = ", ".join(map(lambda municipio: "'"+municipio+"'", municipios)) + " "
+
+	query = "SELECT `NM_MUNICIPIO` AS 'municipio', \
+					`CD_ESTADO_CIVIL` AS 'cd_estado_civil', `DS_ESTADO_CIVIL` AS 'desc_estado_civil', \
+					 SUM(QT_ELEITORES_PERFIL) AS 'soma_eleitores_perfil' \
+					FROM `eleitorado_ATUAL` " +\
+					"WHERE `NM_MUNICIPIO` IN(" + inMunicipios + ") " +\
+					"GROUP BY NM_MUNICIPIO, CD_ESTADO_CIVIL, DS_ESTADO_CIVIL \
+					ORDER BY NM_MUNICIPIO ASC"
+
+	return buscarResultadoPara(query, cursor)
+
+def buscaGrauEscolaridadePorMunicipio(cursor, municipios:[]):
+	inMunicipios = ", ".join(map(lambda municipio: "'"+municipio+"'", municipios)) + " "
+
+	query = "SELECT `NM_MUNICIPIO` AS 'municipio', \
+					`CD_GRAU_ESCOLARIDADE` AS 'cd_grau_escolaridade', `DS_GRAU_ESCOLARIDADE` AS 'desc_grau_escolaridade', \
+					 SUM(QT_ELEITORES_PERFIL) AS 'soma_eleitores_perfil' \
+					FROM `eleitorado_ATUAL` " +\
+					"WHERE `NM_MUNICIPIO` IN(" + inMunicipios + ") " +\
+					"GROUP BY NM_MUNICIPIO, CD_GRAU_ESCOLARIDADE, DS_GRAU_ESCOLARIDADE \
+					ORDER BY NM_MUNICIPIO ASC"
+
+	return buscarResultadoPara(query, cursor)
+
+
 def buscaPorMunicipiosComColunas(cursor, municipios:[], colunas:[]):
 	query = compoeSomaPorColuna(colunas, municipios)
 	return buscarResultadoPara(query, cursor)
 
 def compoeSomaPorColuna(colunas:[], municipios:[]):
 	sumColumns = ', '.join(map(lambda coluna: "sum(`" + coluna + "`) as "+ coluna, colunas)) + " "
-	inMunicipios = ", ".join(map(lambda municipio: "'"+municipio+"'", municipios)) + " " 
-	query = "SELECT `NM_MUNICIPIO`, " +\
+	inMunicipios = ", ".join(map(lambda municipio: "'"+municipio+"'", municipios)) + " "
+
+	query = "SELECT `NM_MUNICIPIO` AS municipio, " +\
 		sumColumns +\
 		"FROM `eleitorado_ATUAL` " +\
 		"WHERE `NM_MUNICIPIO` IN(" + inMunicipios + ") " +\
 		"GROUP BY `NM_MUNICIPIO`"	
 
 	return query
+
 
 def buscarResultadoPara(query:str, cursor):
 	cursor.execute(query)
